c843cdc99863be59a2b3f922ab32fbabd70c7080
孙刚 <1120sungang@gmail.com>
2017-11-12 18:00:55 +0800

    数据类

--- src/main/kotlin/com/william/kt/day2/1-7数据类.kt
+++ src/main/kotlin/com/william/kt/day2/1-7数据类.kt
@@ -6,7 +6,7 @@ package com.william.kt.day2
  * 这里保存并不是物理存储  而是转化为文本格式 便于保存
  * 定义 data class 类名
  *
- * 
+ *
  * Created by sungang on 2017/11/12.
  */
 data class Article(
@@ -18,4 +18,15 @@ fun main(args: Array<String>) {
     val article = Article(1001, "Kotlin学习")
     //数据类的序列化
     println(article.toString())
+
+    //复制 生产一个对象克隆 并更改部分属性值
+    var article2 = article.copy(title = "Kotlin学习2")
+    println(article2.toString())
+
+    //数据类对象解构
+    val (id,title) = article2
+    println("${id},${title}")
+
+    //componentN
+    println("${article2.component2()}")
 }
